2c36e3ac9b831e081c757a9fc1af7185c5a08169
Krishna Soni <krish.soni.02999@gmail.com>
2026-05-27 22:38:10 +0530

    update

--- FRONTEND/videosave/src/index.js
+++ FRONTEND/videosave/src/index.js
@@ -4,9 +4,85 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-reportWebVitals();
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Container,
+  Button,
+} from "react-bootstrap";
+
+export default function CustomNavbar() {
+  return (
+    <Navbar
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      className="py-3 shadow-sm"
+    >
+      <Container>
+
+        <Navbar.Brand
+          href="#"
+          className="fw-bold fs-3 text-danger"
+        >
+          VideoSave
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto align-items-lg-center gap-lg-3">
+
+            <Nav.Link href="#">Home</Nav.Link>
+
+            <Nav.Link href="#">Features</Nav.Link>
+
+            <NavDropdown
+              title="YouTube"
+              id="youtube-dropdown"
+            >
+              <NavDropdown.Item href="#">
+                Video Download
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#">
+                Shorts Download
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#">
+                YouTube to MP3
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown
+              title="Instagram"
+              id="instagram-dropdown"
+            >
+              <NavDropdown.Item href="#">
+                Reel Download
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#">
+                Post Download
+              </NavDropdown.Item>
+
+              <NavDropdown.Item href="#">
+                Story Download
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <Button variant="danger">
+              Download
+            </Button>
+
+          </Nav>
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
+  );
+}
