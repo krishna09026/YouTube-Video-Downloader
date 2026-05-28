b7918937810980e9e28c732905cfd137fe24825f
Krishna Soni <krish.soni.02999@gmail.com>
2026-05-28 23:06:31 +0530

    update

--- FRONTEND/videosave/src/App.js
+++ FRONTEND/videosave/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1> Hello </h1>
+    <h1> Hello world! </h1>
   );
 }
 
---
+++ FRONTEND/videosave/src/Navbar.js
@@ -0,0 +1,11 @@
+// import Navbar from 'App.js';
+
+function Navbar(){
+    return (
+        <>
+        <h1> hello </h1>
+        </>
+    )
+}
+
+export default Navbar
